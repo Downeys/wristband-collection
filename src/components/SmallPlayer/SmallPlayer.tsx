11fcb659dc6f8a61ef9142935ab41547b8cd07be
Downeys <ru.contact.us@gmail.com>
2024-06-25 20:18:07 -0500

    fixes initial track message to not say undefined

--- src/components/SmallPlayer/SmallPlayer.tsx
+++ src/components/SmallPlayer/SmallPlayer.tsx
@@ -4,17 +4,18 @@ import BackButton from "../buttons/BackButton";
 import NextButton from "../buttons/NextButton";
 import PlayButton from "../buttons/PlayButton";
 import Label from "../text/Label";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { PlayListContext } from "@/context/PlayerContext/PlayerContextProvider";
 
 export const SmallPlayer: React.FC = () => {
     const { trackInPlayer, playerStatus, back, next } = useContext(PlayListContext);
+    const trackMessage = useMemo(() => trackInPlayer?.bandName ? `${trackInPlayer?.bandName} - ${trackInPlayer?.trackName}` : 'Welcome to Wristband Radio', [trackInPlayer]);
 
     return (
         <div className="h-44 w-screen flex flex-col pt-2 px-6 bg-slate-950 shadow-footer">
             <div className="flex flex-row justify-between items-center mb-3">
                 <div className="flex flex-row">
-                    <Label text={`${trackInPlayer?.bandName} - ${trackInPlayer?.trackName}`} size="lg" bold />
+                    <Label text={trackMessage} size="lg" bold />
                 </div>
             </div>
             <div className="flex flex-row justify-center items-center p-2">
