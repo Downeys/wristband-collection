58e97fd2a1f0e70eb530399cd2817dbc821d88ce
Downeys <downeys07@gmail.com>
2025-09-27 06:48:26 -0500

    Revert "initializes jest and adds an example test file"

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,4 +1,5 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
+import config from '@/common/config/config';
 
 // Initialize the Auth0 client
 export const auth0 = new Auth0Client({
@@ -12,7 +13,7 @@ export const auth0 = new Auth0Client({
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
-    scope: process.env.AUTH0_SCOPE,
-    audience: process.env.AUTH0_AUDIENCE,
+    scope: config.auth.scope,
+    audience: config.auth.audience,
   },
 });
